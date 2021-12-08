cfd54d33e470a6b4e767af6b92f845681cf6e1e2
Donato Manzione <donato.manz.sc@gmail.com>
2021-12-08 17:16:26 -0500

    Third commit: override toString() method to print all elements of list; make small changes

--- src/lists/Main.java
+++ src/lists/Main.java
@@ -3,9 +3,17 @@ package lists;
 public class Main {
 
 	public static void main(String[] args) {
+
 		SinglyLinkedList<String> singlyList = new SinglyLinkedList<String>("First Element");
+
+		singlyList.addNode("hi");
+		singlyList.addNode("hello");
 		System.out.println(singlyList);
-		
+
+		SinglyLinkedList<Integer> singlyList2 = new SinglyLinkedList<>(12312);
+		singlyList2.addNode(23);
+		singlyList2.addNode(234324);
+		System.out.println(singlyList2);
 
 	}
 
--- src/lists/SinglyLinkedList.java
+++ src/lists/SinglyLinkedList.java
@@ -1,7 +1,9 @@
 package lists;
 
 public class SinglyLinkedList<E> {
-	Node<E> head;
+	private Node<E> head;
+	private int size = 0;
+	private boolean isEmpty = true;
 
 	// Parameterless constructor
 	SinglyLinkedList() {
@@ -11,11 +13,15 @@ public class SinglyLinkedList<E> {
 	// Constructor with parameter to initialize head node
 	SinglyLinkedList(E data) {
 		head = new Node<E>(data);
+		size++;
+		isEmpty = false;
 	}
 
 	// Adding node to list
-	void addNode(Node<E> newNode) {
-		// Create Node variable to store Nodes as we traverse list
+	void addNode(E data) {
+		Node<E> newNode = new Node<E>(data);
+
+		// Create Node variable to store nodes as we traverse list
 		Node<E> traversalNode = head;
 
 		// Traverse list until very last node (that is, the Node object with null
@@ -23,21 +29,55 @@ public class SinglyLinkedList<E> {
 		while (traversalNode.next != null) {
 			traversalNode = traversalNode.next;
 		}
-		
-		//Once you are at last node, add new node as tail 
+
+		// Once you are at last node, add new node as tail
 		traversalNode.next = newNode;
-		
-		
+
+		// Increase size
+		size++;
+
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		// String you will concatenate members to and return.
+		String myList = "SinglyLinkedList: \n\t";
+
+		// Create Node variable to store nodes as we traverse list
+		Node<E> traversalNode = head;
+
+		// If list is not empty, traverse list, convert each node data into String
+		// object, concatenate
+		if (!isEmpty) {
+			for (int i = 0; i < size; i++) {
+				myList += traversalNode;
+				if (i < size - 1) {
+					myList += "-> ";
+				}
+				traversalNode = traversalNode.next;
+			}
+		}
+
+		// Return String representation of linked list
+
+		return myList;
+	}
+
 }
 
 class Node<E> {
+
 	Node(E data) {
 		this.data = data;
 	}
 
 	E data;
 	Node<E> next;
+
+	@Override
+	public String toString() {
+		// Return data member converted to String
+		return String.valueOf(data);
+	}
+
 }
