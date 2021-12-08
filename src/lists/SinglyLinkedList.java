78029e394678cfbdb0316d3d9cadd9a9ebb99675
Donato Manzione <donato.manz.sc@gmail.com>
2021-12-08 16:16:21 -0500

    Second commit: add insertion method to singly linked list

--- src/lists/SinglyLinkedList.java
+++ src/lists/SinglyLinkedList.java
@@ -2,19 +2,42 @@ package lists;
 
 public class SinglyLinkedList<E> {
 	Node<E> head;
-	
-	SinglyLinkedList(){
-		
+
+	// Parameterless constructor
+	SinglyLinkedList() {
+
 	}
-	SinglyLinkedList(E data){
+
+	// Constructor with parameter to initialize head node
+	SinglyLinkedList(E data) {
 		head = new Node<E>(data);
 	}
+
+	// Adding node to list
+	void addNode(Node<E> newNode) {
+		// Create Node variable to store Nodes as we traverse list
+		Node<E> traversalNode = head;
+
+		// Traverse list until very last node (that is, the Node object with null
+		// instance variable called next
+		while (traversalNode.next != null) {
+			traversalNode = traversalNode.next;
+		}
+		
+		//Once you are at last node, add new node as tail 
+		traversalNode.next = newNode;
+		
+		
+	}
+	
+	
 }
 
 class Node<E> {
-	Node(E data){
+	Node(E data) {
 		this.data = data;
 	}
+
 	E data;
 	Node<E> next;
 }
