8beb48d9acf26ebf1d41416dc548a15302517c17
Donato Manzione <donato.manz.sc@gmail.com>
2021-12-09 12:33:09 -0500

    added addition and removal operations in linked list and fixed some bugs

--- src/lists/Main.java
+++ src/lists/Main.java
@@ -4,17 +4,42 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// Check creation of list with parameter
 		SinglyLinkedList<String> singlyList = new SinglyLinkedList<String>("First Element");
 
-		singlyList.addNode("hi");
-		singlyList.addNode("hello");
+		// Check addition of new elements to list
+		singlyList.add("hi");
+		singlyList.add("hello");
+		singlyList.add("ciao");
 		System.out.println(singlyList);
 
-		SinglyLinkedList<Integer> singlyList2 = new SinglyLinkedList<>(12312);
-		singlyList2.addNode(23);
-		singlyList2.addNode(234324);
+		// Check creation of list without parameters and addition of elements to said
+		// list
+
+		SinglyLinkedList<Integer> singlyList2 = new SinglyLinkedList<>();
+		singlyList2.add(23);
+		singlyList2.add(234324);
 		System.out.println(singlyList2);
 
+		// Test element removal when there are 2 or more elements and element is in list
+		singlyList.remove("hi");
+		System.out.println(singlyList);
+
+		// Test element removal when there are 2 or more elements and element is NOT in
+		// list
+		singlyList.remove("hallo");
+		System.out.println(singlyList);
+
+		// Test removal of several elements at once
+		singlyList.remove("First Element", "ciao", "hi");
+		System.out.println(singlyList);
+
+		// Test element removal when the list is empty
+		singlyList.remove("any");
+
+		// Test addition of several elements at once
+		singlyList.add("First Element", "hi", "hello", "ciao");
+		System.out.println(singlyList);
 	}
 
 }
--- src/lists/SinglyLinkedList.java
+++ src/lists/SinglyLinkedList.java
@@ -1,7 +1,7 @@
 package lists;
 
 public class SinglyLinkedList<E> {
-	private Node<E> head;
+	private Node head;
 	private int size = 0;
 	private boolean isEmpty = true;
 
@@ -12,30 +12,121 @@ public class SinglyLinkedList<E> {
 
 	// Constructor with parameter to initialize head node
 	SinglyLinkedList(E data) {
-		head = new Node<E>(data);
+		head = new Node(data);
 		size++;
 		isEmpty = false;
 	}
 
 	// Adding node to list
-	void addNode(E data) {
-		Node<E> newNode = new Node<E>(data);
+	void add(E... dataVarArg) {
+		
+		//Perform add operation for each argument provided
 
-		// Create Node variable to store nodes as we traverse list
-		Node<E> traversalNode = head;
+		for (E data : dataVarArg) {
+			Node newNode = new Node(data);
 
-		// Traverse list until very last node (that is, the Node object with null
-		// instance variable called next
-		while (traversalNode.next != null) {
-			traversalNode = traversalNode.next;
+			if (isEmpty) {
+				// If list is empty, simply make new node the head node
+				head = newNode;
+
+				// Update information
+				isEmpty = false;
+
+			} else {
+
+				// Create Node variable to store nodes as we traverse list
+				Node traversalNode = head;
+
+				// Traverse list until very last node (that is, the Node object with null
+				// instance variable called next
+				while (traversalNode.next != null) {
+					traversalNode = traversalNode.next;
+				}
+
+				// Once you are at last node, add new node as tail
+				traversalNode.next = newNode;
+			}
+
+			// Increase size
+			size++;
 		}
 
-		// Once you are at last node, add new node as tail
-		traversalNode.next = newNode;
+	}
 
-		// Increase size
-		size++;
+	// Remove node from list. Remove first node with given data
+	void remove(E... dataVarArg) {
 
+		//Perform remove operation for each argument provided
+		argIteration: for (E data : dataVarArg) {
+
+			// Account for when list is empty
+			if (isEmpty) {
+				System.out.println("List is empty. Nothing to remove.");
+				//Stop iterating through arguments
+				break argIteration;
+			}
+
+			// Account for when there is only one element in list
+			if (size == 1) {
+				if (head.data == data) {
+					head = null;
+
+					// Update information about list
+					isEmpty = true;
+					size--;
+
+				} else {
+					System.out.println(data + " is not in list.");
+				}
+
+				// Continue iterating through arguments
+				continue argIteration;
+			}
+
+			Node traversalNode = head;
+			Node traversalNode2 = head.next;
+
+			
+			// Account for when there are 2 or more elements and first is a match
+			if(traversalNode.data == data) {
+				head = traversalNode2;
+				
+				// Update information about list
+				size--;
+
+				
+				// Continue iterating through arguments
+				continue argIteration;
+			}
+			
+			
+			// Iterate through list
+			for (int i = 0; i < size; i++) {
+
+				// Check if data of node matches what you are looking for
+				if (traversalNode2.data == data) {
+
+					// NullPointerException? But it is not being used, just assigned.
+					traversalNode2.next = traversalNode2.next;
+
+					// Update information about list
+					size--;
+
+					// Continue iterating through arguments
+					continue argIteration;
+
+				} else {
+
+					Node temp = traversalNode2.next;
+					traversalNode = traversalNode2;
+					traversalNode2 = temp;
+				}
+
+			}
+
+			// If no such element exists
+			System.out.println(data + "is not in list.");
+		}
 	}
 
 	@Override
@@ -44,7 +135,7 @@ public class SinglyLinkedList<E> {
 		String myList = "SinglyLinkedList: \n\t";
 
 		// Create Node variable to store nodes as we traverse list
-		Node<E> traversalNode = head;
+		Node traversalNode = head;
 
 		// If list is not empty, traverse list, convert each node data into String
 		// object, concatenate
@@ -56,28 +147,32 @@ public class SinglyLinkedList<E> {
 				}
 				traversalNode = traversalNode.next;
 			}
+			return myList;
+			
+		}else {
+			return myList + "[[empty]]";
 		}
 
 		// Return String representation of linked list
 
-		return myList;
+		
 	}
 
-}
+	private class Node {
 
-class Node<E> {
+		Node(E data) {
+			this.data = data;
+		}
 
-	Node(E data) {
-		this.data = data;
-	}
+		E data;
+		Node next;
 
-	E data;
-	Node<E> next;
+		@Override
+		public String toString() {
+			// Return data member converted to String
+			return String.valueOf(data);
+		}
 
-	@Override
-	public String toString() {
-		// Return data member converted to String
-		return String.valueOf(data);
 	}
 
 }
